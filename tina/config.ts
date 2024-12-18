5a97e0879d7edf95dec3f9e5606b2009b4ac30dd
manavees <60441073+manavees@users.noreply.github.com>
2024-12-18 18:14:55 +0200

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'tinacms';
 
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 export default defineConfig({
-  branch: 'master', // Replace 'main' with the branch you want to edit
+  branch,
   clientId: '2a9e6685-ef4f-4038-9c55-acdc6c6b238a', // Replace with your TinaCMS Cloud Client ID
   token: '65dbf776027761e132ee03f5ee88f5247ccf3f08', // Replace with your TinaCMS Cloud Token
   build: {
