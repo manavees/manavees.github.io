25f9f13d271def8d66805c845ef2ecd5cbf4503f
manavees <60441073+manavees@users.noreply.github.com>
2024-12-18 17:38:29 +0200

    Create config.ts

---
+++ tina/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'tinacms';
+
+export default defineConfig({
+  branch: 'main', // Replace 'main' with the branch you want to edit
+  clientId: '2a9e6685-ef4f-4038-9c55-acdc6c6b238a', // Replace with your TinaCMS Cloud Client ID
+  token: '65dbf776027761e132ee03f5ee88f5247ccf3f08', // Replace with your TinaCMS Cloud Token
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'static',
+  },
+  schema: {
+    collections: [
+      {
+        name: 'posts',
+        label: 'Posts',
+        path: 'content/posts',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+          },
+        ],
+      },
+    ],
+  },
+});
