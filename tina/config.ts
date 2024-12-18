2bb75713d5552dcd88e4ca15b6e10d87f8a0b9d9
manavees <60441073+manavees@users.noreply.github.com>
2024-12-18 17:55:33 +0200

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tinacms';
 
 export default defineConfig({
-  branch: 'main', // Replace 'main' with the branch you want to edit
+  branch: 'master', // Replace 'main' with the branch you want to edit
   clientId: '2a9e6685-ef4f-4038-9c55-acdc6c6b238a', // Replace with your TinaCMS Cloud Client ID
   token: '65dbf776027761e132ee03f5ee88f5247ccf3f08', // Replace with your TinaCMS Cloud Token
   build: {
