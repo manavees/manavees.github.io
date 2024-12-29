a07c4f405ec94d0c1df59934d175bab8062520e6
manavees <60441073+manavees@users.noreply.github.com>
2024-12-29 11:45:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import solidJs from "@astrojs/solid-js"
 // https://astro.build/config
 export default defineConfig({
   site: "https://manavees.github.io",
-  integrations: [tinacms(), mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
 })
