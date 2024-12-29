6a4f4e2b5c44bb7b3d3ca794d47fe9917e48e240
manavees <60441073+manavees@users.noreply.github.com>
2024-12-29 11:48:01 +0200

    Create config.js

---
+++ src/cms/config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main", // The branch where content changes are committed
+  clientId: process.env.TINA_CLIENT_ID, // Tina.io Client ID
+  token: process.env.TINA_TOKEN, // Tina.io Token
+  media: {
+    tina: {
+      mediaRoot: "public/images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        label: "Pages",
+        name: "pages",
+        path: "src/pages",
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+          },
+          {
+            type: "rich-text",
+            label: "Content",
+            name: "body",
+          },
+        ],
+      },
+    ],
+  },
+});
