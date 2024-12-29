11f68420a834428d880313a8b6875ab109d45879
manavees <60441073+manavees@users.noreply.github.com>
2024-12-29 12:20:12 +0200

    Update config.js

--- src/tina/config.js
+++ src/tina/config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "tinacms";
+import { defineConfig, TinaCMSConfig } from "tinacms";
 
-export default defineConfig({
-  branch: "main", // The branch where content changes are committed
-  clientId: process.env.TINA_CLIENT_ID || "", // Ensure this is a valid string
-  token: process.env.TINA_TOKEN || "", // Ensure this is a valid string
+const tinaConfig: TinaCMSConfig = defineConfig({
+  branch: "main",
+  clientId: process.env.TINA_CLIENT_ID || "",
+  token: process.env.TINA_TOKEN || "",
   media: {
     tina: {
       mediaRoot: "public/images",
@@ -17,18 +17,12 @@ export default defineConfig({
         name: "pages",
         path: "src/pages",
         fields: [
-          {
-            type: "string",
-            label: "Title",
-            name: "title",
-          },
-          {
-            type: "rich-text",
-            label: "Content",
-            name: "body",
-          },
+          { type: "string", label: "Title", name: "title" },
+          { type: "rich-text", label: "Content", name: "body" },
         ],
       },
     ],
   },
 });
+
+export default tinaConfig;
