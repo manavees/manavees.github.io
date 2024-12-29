078e533f3cd130618ceccbfa2a4f5adf765617f4
manavees <60441073+manavees@users.noreply.github.com>
2024-12-29 14:10:26 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import solidJs from "@astrojs/solid-js";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import solidJs from "@astrojs/solid-js"
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://manavees.github.io",
+  site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
-  vite: {
-    define: {
-      "process.env.TINA_CLIENT_ID": JSON.stringify(process.env.TINA_CLIENT_ID),
-      "process.env.TINA_TOKEN": JSON.stringify(process.env.TINA_TOKEN),
-    },
-  },
-});
+})
